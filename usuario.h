479853228908e8aa7f14d58d4159c5dcfdc5fc22
Rpsneto <rpsneto@icloud.com>
2026-09-25 12:26:02 -0300

    codigo do usuario

---
+++ usuario.h
@@ -0,0 +1,15 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+typedef struct {
+    int matricula;
+    char nome[100];
+    char curso[50];
+    int total_emprestimos; 
+} Usuario;
+
+void cadastrar_usuario(Usuario lista[], int *total);
+void listar_usuarios(Usuario lista[], int total);
+int buscar_usuario_matricula(Usuario lista[], int total, int matricula);
+
+#endif
