9a141e1face320fda7c50d2e0bb3b8ec4e802af6
Rpsneto <rpsneto@icloud.com>
2026-09-25 12:35:41 -0300

    Finalmente, a main foi finalizada, uma parte muito chata foi entender o case

--- sistema.c
+++ sistema.c
@@ -1,4 +1,83 @@
-//inicio do meu trabalho de biblioteca 
 
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
+#include "livro.h"
+#include "usuario.h"
+#include "emprestimo.h"
+
+int main() {
+   
+    Livro lista_livros[100];
+    
+    int total_livros = 0;
+
+    Usuario lista_usuarios[100];
+    int total_usuarios = 0;
+
+   
+    Emprestimo lista_emprestimos[100];
+    int total_emprestimos = 0;
+
+   
+    int opcao;
+    int matricula_busca;
+
+   
+    do {
+        printf("\n===================================\n");
+        printf("       SISTEMA DA BIBLIOTECA       \n");
+        printf("===================================\n");
+        printf("1 - Cadastrar Livro\n");
+        printf("2 - Listar Livros\n");
+        printf("3 - Cadastrar Usuario\n");
+        printf("4 - Listar Usuarios\n");
+        printf("5 - Registrar Emprestimo\n");
+        printf("6 - Registrar Devolucao (Com Multas)\n");
+        printf("7 - Listar Emprestimos\n");
+        printf("8 - Historico por Usuario\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        
+    
+        scanf("%d", &opcao);
+        while (getchar() != '\n');
+
+    
+        switch(opcao) {
+            case 1:
+                cadastrar_livro(lista_livros, &total_livros);
+                break;
+            case 2:
+                listar_livros(lista_livros, total_livros);
+                break;
+            case 3:
+                cadastrar_usuario(lista_usuarios, &total_usuarios);
+                break;
+            case 4:
+                listar_usuarios(lista_usuarios, total_usuarios);
+                break;
+            case 5:
+                registrar_emprestimo(lista_emprestimos, &total_emprestimos, lista_usuarios, total_usuarios, lista_livros, total_livros);
+                break;
+            case 6:
+                registrar_devolucao(lista_emprestimos, total_emprestimos, lista_livros, total_livros);
+                break;
+            case 7:
+                listar_emprestimos(lista_emprestimos, total_emprestimos);
+                break;
+            case 8:
+                printf("Digite a matricula para buscar o historico: ");
+                scanf("%d", &matricula_busca);
+                while (getchar() != '\n');
+                historico_por_usuario(lista_emprestimos, total_emprestimos, matricula_busca);
+                break;
+            case 0:
+                printf("\nSaindo do sistema... Ate logo!\n");
+                break;
+            default:
+                printf("\nOpcao invalida! Tente novamente.\n");
+        }
+    } while(opcao != 0);
+
+    return 0;
+}
